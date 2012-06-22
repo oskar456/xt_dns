15e61012f6a9e7aeb0a88b626224390c77d53d13
Ondřej Caletka <ondrej@caletka.cz>
2012-06-22 16:02:17 +0200

    Match EDNS0 more precisely.

--- xt_dns.c
+++ xt_dns.c
@@ -77,7 +77,8 @@ static bool dns_mt(const struct sk_buff *skb, struct xt_action_param *par)
 		&& (dns[4] == 0x00) && (dns[5] == 0x01) && (dns[6] == 0x00) && (dns[7] == 0x00)
 		&& (dns[8] == 0x00) && (dns[9] == 0x00) && (dns[10] == 0x00) && (dns[11] == 0x01)
 		&& (dns[len-16] == 0x00) && (dns[len-15] == 0x00) && (dns[len-14] == info->type)
-		&& (dns[len-13] == 0x00) && (dns[len-12] == 0x01)
+		&& (dns[len-13] == 0x00) && (dns[len-12] == 0x01) && (dns[len-11] == 0x00)
+		&& (dns[len-10] == 0x00) && (dns[len-9] == 0x29)
 		&& (dns[len-2] == 0x00) && (dns[len-1] == 0x00);
 	return (is_match ^ info->invert) ? true : false;
 }
